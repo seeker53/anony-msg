262b720f042014ce94f8ef37da7250524237cde2
seeker53 <dishant.singh16@gmail.com>
2024-09-02 22:30:25 +0530

    user model created

---
+++ src/models/user.model.ts
@@ -0,0 +1,54 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { Message } from './message.model';
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    isVerified: boolean;
+    verifyCodeExpiry: Date;
+    isAcceptingMessages: boolean;
+    messages: mongoose.Types.ObjectId[]; // Reference to Message IDs
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        trim: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, 'Verify code is required'],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, 'Verify code expiry is required'],
+    },
+    isAcceptingMessages: {
+        type: Boolean,
+        default: true
+    },
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }] // Referencing Message IDs
+});
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>('User', UserSchema));
+
+export default UserModel;
