1954d4c187ca9e05e059fe74325fcef596d4b0f3
seeker53 <dishant.singh16@gmail.com>
2024-10-08 00:23:10 +0530

    verification mail template changed

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,9 +9,9 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
     try {
         await resend.emails.send({
-            from: 'onboarding@resend.dev',
+            from: 'noreply@whispernet.com',
             to: email,
-            subject: 'Mystery Message | Verification Code',
+            subject: 'Anonymous Message | Verification Code',
             react: VerificationEmail({ username, otp: verifyCode }),
         });
         return { success: true, message: "Verification sent successfully!" }
